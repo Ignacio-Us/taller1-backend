a2fe1ecb87b4127727802d2d5ab665f9659546cb
Ignacio-Us <i.uribe01@ufromail.cl>
2024-06-05 15:26:53 -0400

    creacion de modelo Campeon

---
+++ src/main/java/ufro/ignaciou/taller1_backend/models/Campeon.java
@@ -0,0 +1,41 @@
+package ufro.ignaciou.taller1_backend.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "campeon")
+public class Campeon {
+    
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    @Column(name = "id_campeon")
+    private Long idCampeon;
+    
+    @Column(name = "tipo_ataque")
+    private String tipoAtaque;
+
+    @Column(name = "vida_base")
+    private Long vidaBase;
+
+    @Column(name = "mana_bool")
+    private Short manaBool;
+
+    @Column(name = "energia_bool")
+    private Short energiaBool;
+
+    @Column(name = "velocidad_ataque_base")
+    private Float velocidadAtaqueBase;
+
+    @Column(name = "velocidad_movimiento_base")
+    private Float velocidadMovimientoBase;
+
+    @Column(name = "nombre")
+    private String nombre;
+}
