c33a50c08b4817356d83fde6df0675b46739c2cb
Ignacio-Us <i.uribe01@ufromail.cl>
2024-06-05 12:43:34 -0400

    Initial commit

---
+++ src/test/java/ufro/ignaciou/taller1_backend/Taller1BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ufro.ignaciou.taller1_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Taller1BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
